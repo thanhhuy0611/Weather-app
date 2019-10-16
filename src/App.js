35eb712dd4f19155c839df4af55e2dddc1ed6567
= <=>
2019-10-16 17:44:13 +0700

    revise

--- src/App.js
+++ src/App.js
@@ -75,13 +75,13 @@ function App() {
 
 <div className="waveWrapper waveAnimation">
         <div className="waveWrapperInner bgTop">
-          <div className="wave waveTop" style={{backgroundImage: 'url("http://front-end-noobs.com/jecko/img/wave-top.png")'}} />
+          <div className="wave waveTop" style={{backgroundImage: 'url("https://front-end-noobs.com/jecko/img/wave-top.png")'}} />
         </div>
         <div className="waveWrapperInner bgMiddle">
-          <div className="wave waveMiddle" style={{backgroundImage: 'url("http://front-end-noobs.com/jecko/img/wave-mid.png")'}} />
+          <div className="wave waveMiddle" style={{backgroundImage: 'url("https://front-end-noobs.com/jecko/img/wave-mid.png")'}} />
         </div>
         <div className="waveWrapperInner bgBottom">
-          <div className="wave waveBottom" style={{backgroundImage: 'url("http://front-end-noobs.com/jecko/img/wave-bot.png")'}} />
+          <div className="wave waveBottom" style={{backgroundImage: 'url("https://front-end-noobs.com/jecko/img/wave-bot.png")'}} />
         </div>
       </div>
       </div>
